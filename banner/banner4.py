3707df9d337216ccd2d9b2fe72f764e9ca7a8c0d
syafiqm7 <39830823+syafiqm7@users.noreply.github.com>
2018-06-10 17:00:09 +0800

    Update banner4.py

--- banner/banner4.py
+++ banner/banner4.py
@@ -2,7 +2,7 @@
 L = '\033[1;36m' #L
 print ""+L+""
 print " ____________"
-print "< darksploit >"
+print "< CODE ARTHUR >"
 print " ------------"
 print "       \   ,__,"
 print "        \  (oo)____"
